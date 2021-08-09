e77472f1faf88870ad6653dfc55ce33d95bb1b69
Tarique Smith <tpsmith1992@gmail.com>
2021-08-09 17:03:16 -0400

    commit tree 669dd392b0b11c2266cbb086f8f1d4aa5dbacccf

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  // const imgalt= {
+  //  alt : "I made this"
+  //}
+  return (
+  <div id='about'>About
+  <h2>About Me</h2>
+  <p>I like learning!</p>
+  <img src={image} alt="I made this"></img></div>
+  )
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,14 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  const divStyle ={
+    color: 'firebrick'
+  }
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id='home'>Home
+    <h1 style={{color: 'firebrick'}}>{name} is a Web Developer from {city}</h1>
+    <h2>{name} from {city}</h2>
+  </div>;
 }
 
 export default Home;
